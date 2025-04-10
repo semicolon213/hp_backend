a282b45a222ea49ec0b1f9ca677f75e5df6cba22
semicolon213 <khk213624@gmail.com>
2025-04-10 16:03:55 +0900

    Init Commit

---
+++ src/main/java/com/hot_potato/hp_backend/HpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hot_potato.hp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HpBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hot_potato/hp_backend/HpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hot_potato.hp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
